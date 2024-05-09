fa9765ffe7e0ad35acaa32aeffbaf81f1284b272
russell-brouillard <rbrouillard@crunchtime.com>
2024-05-09 16:50:22 -0600

    save

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,15 +1,14 @@
 import { getAuth } from "@firebase/auth";
 import { initializeApp } from "firebase/app";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA2tdOulU73ADVdPhrnxQYt570L4ZBRQFQ",
-  authDomain: "pomerene-dev.firebaseapp.com",
-  projectId: "pomerene-dev",
-  storageBucket: "pomerene-dev.appspot.com",
-  messagingSenderId: "844147711427",
-  appId: "1:844147711427:web:2386f0061923f540bdf176",
-  measurementId: "G-6T3CY94SZB"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
